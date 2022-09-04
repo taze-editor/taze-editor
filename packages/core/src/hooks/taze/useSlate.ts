da3d313a1487068d74fe6f6ce82563fe8657abd4
Kerim <kerimcetintas@gmail.com>
2022-09-04 16:59:43 +0300

    feat: add typed `useSlate` hook
    it needs to be called in a child component of `Taze`

---
+++ packages/core/src/hooks/taze/useSlate.ts
@@ -0,0 +1,13 @@
+import { useSlate as useSlateReact } from "slate-react";
+import { Value } from "../../slate/editor/TEditor";
+import { TazeEditor } from "../../types/taze/TazeEditor";
+
+/**
+ * Typed {@link useSlate} & TazeEditor.
+ * Needs to be called in a child component of `Taze`.
+ */
+
+export const useSlate = <
+  V extends Value = Value,
+  E extends TazeEditor<V> = TazeEditor<V>
+>() => (useSlateReact() as unknown) as E;
