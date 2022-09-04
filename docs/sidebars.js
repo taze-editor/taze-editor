b4c065ac886b66fce481a72b48d40e6fc21d1477
Kerim <kerimcetintas@gmail.com>
2022-09-04 19:19:00 +0300

    docs: add create toolbar example

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -14,7 +14,8 @@ const sidebars = {
       items: [
         "getting-started/installation",
         "getting-started/create-editor",
-        "getting-started/basic-plugins"
+        "getting-started/basic-plugins",
+        "getting-started/create-toolbar"
       ]
     }
   ]
