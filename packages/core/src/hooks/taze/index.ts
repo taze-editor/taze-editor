bfdf0e6a7ac88bfd6793d691a7631651d5a9ff5f
Kerim <kerimcetintas@gmail.com>
2022-09-07 01:59:15 +0300

    Add barrels

--- packages/core/src/hooks/taze/index.ts
+++ packages/core/src/hooks/taze/index.ts
@@ -4,9 +4,9 @@
 
 export * from './useDecorate';
 export * from './useEditableProps';
+export * from './useEditorState';
 export * from './useOnChange';
 export * from './useRenderElement';
 export * from './useRenderLeaf';
-export * from './useSlate';
 export * from './useSlateProps';
 export * from './useTaze';
