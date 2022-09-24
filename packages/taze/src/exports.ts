453da120e98da9872f2107628daf78a6d03cfa2e
Kerim <kerimcetintas@gmail.com>
2022-09-24 17:41:46 +0300

    fix: taze `balloon-toolbar` export

--- packages/taze/src/exports.ts
+++ packages/taze/src/exports.ts
@@ -2,3 +2,4 @@ export * from "@taze-editor/taze-core";
 export * from "@taze-editor/taze-plugin-basic-elements";
 export * from "@taze-editor/taze-plugin-basic-marks";
 export * from "@taze-editor/taze-plugin-search-highlight";
+export * from "@taze-editor/taze-ui-balloon-toolbar";
