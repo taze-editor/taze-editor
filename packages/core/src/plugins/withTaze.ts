b5377c3a042119959c625aa53d054a7ac6887ff0
Kerim <kerimcetintas@gmail.com>
2022-10-02 20:16:29 +0300

    feat: implement plugin helpers & override options

--- packages/core/src/plugins/withTaze.ts
+++ packages/core/src/plugins/withTaze.ts
@@ -1,33 +1,37 @@
 import { TazeProps } from "../components";
 import { TEditor, Value } from "../slate/editor/TEditor";
-import { PluginOptions, TazePlugin } from "../types/plugin/TazePlugin";
 import { TazeEditor } from "../types/taze/TazeEditor";
+import { setTazePlugins } from "../utils/taze/setTazePlugins";
 
 export interface WithTazeOptions<
   V extends Value = Value,
   E extends TazeEditor<V> = TazeEditor<V>
-> extends Pick<TazeProps<V, E>, "plugins"> {}
+> extends Pick<TazeProps<V, E>, "plugins" | "disableCorePlugins"> {}
 
 export const withTaze = <
   V extends Value = Value,
   E extends TEditor<V> = TEditor<V>
 >(
-  editor: E,
-  {
-    plugins
-  }: {
-    plugins?: TazePlugin<PluginOptions, V>[];
-  } = {}
+  e: E,
+  { plugins, disableCorePlugins }: WithTazeOptions<V, E & TazeEditor<V>> = {}
 ): E & TazeEditor<V> => {
-  let e = editor as any as E & TazeEditor<V>;
+  let editor = (e as any) as E & TazeEditor<V>;
 
-  if (!e.key) {
-    e.key = Math.random();
+  if (!editor.key) {
+    editor.key = Math.random();
   }
 
-  if (plugins) {
-    e.plugins = plugins;
-  }
+  setTazePlugins<V>(editor, {
+    plugins: plugins as any,
+    disableCorePlugins
+  });
 
-  return e;
+  // withOverrides
+  editor.plugins.forEach(plugin => {
+    if (plugin.withOverrides) {
+      editor = plugin.withOverrides(editor, plugin) as any;
+    }
+  });
+
+  return editor;
 };
