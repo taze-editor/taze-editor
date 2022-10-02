38572d8364268ee18792d995e6e027d17093ad05
Kerim <kerimcetintas@gmail.com>
2022-10-02 20:07:54 +0300

    feat: add `pluginsByKey` option for TazeEditor

--- packages/core/src/types/taze/TazeEditor.ts
+++ packages/core/src/types/taze/TazeEditor.ts
@@ -1,6 +1,7 @@
 import { TEditor, Value } from "../../slate/editor/TEditor";
 import { THistoryEditor } from "../../slate/history-editor/THistoryEditor";
 import { TReactEditor } from "../../slate/react-editor/TReactEditor";
+import { PluginKey } from "../plugin";
 import { WithTazePlugin } from "../plugin/TazePlugin";
 
 export type TazeEditor<V extends Value = Value> = TEditor<V> &
@@ -9,4 +10,5 @@ export type TazeEditor<V extends Value = Value> = TEditor<V> &
     key: any;
     id: string;
     plugins: WithTazePlugin<{}, V>[];
+    pluginsByKey: Record<PluginKey, WithTazePlugin<{}, V>>;
   };
