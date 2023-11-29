82a26400aa2c890a8bef4ce8e9878923962f752d
Kerim <kerimcetintas@gmail.com>
2023-11-29 23:42:55 +0300

    feat(plugin): add `onChange` event for search highlight plugin

--- packages/taze-plugins/search-highlight/src/createSearchHighlightPlugin.ts
+++ packages/taze-plugins/search-highlight/src/createSearchHighlightPlugin.ts
@@ -19,5 +19,31 @@ export const createSearchHighlightPlugin = createPluginFactory<
   renderAfterEditable: SearchHighlightEffect,
   component: SearchHighlightComponent,
   store: useSearchHighlightPluginStore,
-  decorate: decorateSearchHighlight
+  decorate: decorateSearchHighlight,
+  onChange: (editor, store) => _value => {
+    if (!store) {
+      console.warn(
+        "[@taze-editor/taze-plugin-search-highlight] store is not defined @onChange"
+      );
+      return;
+    }
+
+    const { operations } = editor;
+    const firstOperation = operations[0];
+
+    if (
+      firstOperation &&
+      (firstOperation.type === "insert_text" ||
+        firstOperation.type === "remove_text")
+    ) {
+      const {
+        getAllSearchRanges,
+        setSearchMatchedRanges,
+        searchParams
+      } = store.getState();
+
+      const ranges = getAllSearchRanges(editor, searchParams);
+      setSearchMatchedRanges(ranges);
+    }
+  }
 });
